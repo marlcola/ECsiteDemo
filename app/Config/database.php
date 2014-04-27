57f76313a7f7f65884c22138e93693d3ce42d008
masaki <marlcola2@gmail.com>
2014-04-27 17:16:54 +0900

    無視されていたtmpディレクトリを追加しました

---
+++ app/Config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+class DATABASE_CONFIG {
+
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'root',
+        'password' => 'cola3101',
+        'database' => 'ec_site',
+        'prefix' => '',
+        'encoding' => 'utf8'
+    );
+//    
+//    public $default = array(
+//        'datasource' => 'Database/Mysql',
+//        'persistent' => false,
+//        'host' => 'localhost',
+//        'login' => 'root',
+//        'password' => 'cola3101',
+//        'database' => 'ec_site_test',
+//        'prefix' => '',
+//        'encoding' => 'utf8'
+//    );
+}
